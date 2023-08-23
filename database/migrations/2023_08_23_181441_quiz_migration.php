c506d0b7b21774d928f0c9713f93e14f96039920
Mohamed Elazab <elazabmuhamed@gmail.com>
2023-08-24 00:26:25 +0300

    quiz migration

---
+++ database/migrations/2023_08_23_181441_quiz_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description')->nullable();
+            $table->timestamp('date')->nullable();
+            $table->double('duration')->nullable();
+            $table->double('passing_score')->nullable();
+            $table->enum('status', ['publish','draft', 'passive'])->default('draft');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
