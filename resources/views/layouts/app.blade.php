a28d85d96608ca83be6dc6ae14d12ceed8204cf0
Mohamed Elazab <elazabmuhamed@gmail.com>
2023-08-23 16:06:26 +0300

    storage symlink and bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title> {{$header}} - {{'Sınav Sistemi'}}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -27,15 +27,21 @@
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $header }}
+        </h2>
                     </div>
                 </header>
             @endif
 
             <!-- Page Content -->
-            <main>
+
+            <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 {{ $slot }}
-            </main>
+        </div>
+    </div>
+
         </div>
 
         @stack('modals')
