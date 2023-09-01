ba474d690c10e17f052e25c72037b1a4aa896a2b
Mohamed Elazab <elazabmuhamed@gmail.com>
2023-09-01 17:09:51 +0300

    answer and result tables and seeders added success message after taking quiz added

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quiz;
+use App\Models\Answer;
+use App\Models\Result;
 
 use Illuminate\Http\Request;
 
@@ -21,8 +23,43 @@ class MainController extends Controller
     }
 
     public function quiz_detail($slug){
-
         $quiz= Quiz::whereSlug($slug)->withCount('questions')->first()?? abort(404, 'Quiz not found.');
         return view('quiz_detail', compact('quiz'));
+    }
+
+    public function result(Request $request, $slug){
+        
+        $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404, 'Quiz not found.');
+        $correct=0;
+        //dd([$quiz, $request->post()]);
+        //return $request->post();
+
+        foreach($quiz->questions as $question){
+            //echo $question->id.'-'.$question->correct_answer.'/'.$request->post($request->id).'<br>';
+            Answer::create([
+                'user_id'=>auth()->user()->id,
+                'question_id'=>$question->id,
+                'answer'=>$request->post($question->id)
+            ]);
+
+
+            if($question->correct_answer==$request->post($question->id)){
+                $correct +=1;
+            }
+        }
+
+        $score = round((100/count($quiz->questions))*$correct); 
+        $wrong = count($quiz->questions)-$correct;
+
+        Result::create([
+            'user_id'=>auth()->user()->id,
+            'quiz_id'=>$quiz->id,
+            'score'=>$score,
+            'correct'=>$correct,
+            'wrong'=>$wrong,
+        ]);
+
+        return redirect()->route('quiz.detail', $quiz->slug)->withSuccess('Quiz was taken successfully. Your score is: '.$score);
+
     }
 }
